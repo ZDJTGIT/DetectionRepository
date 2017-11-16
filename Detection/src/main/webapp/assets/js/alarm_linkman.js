7ce66e9c044b628d6880323466f5ba716fa22463
liIverson <liIverson@nba.com>
2017-11-16 10:04:36 +0800

    提示框

--- Detection/src/main/webapp/assets/js/alarm_linkman.js
+++ Detection/src/main/webapp/assets/js/alarm_linkman.js
@@ -13,17 +13,36 @@ $('#addAlarmBasic').on('click','.deletebutton',function(){
         table.$('tr.selected').removeClass('selected');
         parenttr.addClass('selected');
     }
-	$.ajax({
-		url:"rest/project/deletealarm",
-		data:{alarmLinkmanId:deleteid},
-		success:function(data){
-			if(data=="true"){
-				table.row('.selected').remove().draw( false );
-			}else{
-				alert("删除失败");
-			}
-		}
-	});
+	swal({
+        title: "您确定要删除这条信息吗",
+        text: "删除后将无法恢复，请谨慎操作！",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "是的，我要删除！",
+        cancelButtonText: "让我再考虑一下…",
+        closeOnConfirm: false,
+        closeOnCancel: false
+    },function (isConfirm) {
+        if (isConfirm) {
+        	$.ajax({
+        		url:"rest/project/deletealarm",
+        		data:{alarmLinkmanId:deleteid},
+        		success:function(data){
+        			if(data=="true"){
+        				swal("删除成功！", "您已经永久删除了这条信息。", "success");
+        				table.row('.selected').remove().draw( false );
+        			}else{
+        				swal("删除失败", "请您重新尝试。", "error");
+        			}
+        		}
+        	});
+            
+        } else {
+            swal("已取消", "您取消了删除操作！", "error");
+        }
+    });
+
 	
 });
 
