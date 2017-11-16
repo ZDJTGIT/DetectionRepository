b2ec5ecd9f1b09282b1b9ef0d8364cc2a02c773a
zemin <731583657@qq.com>
2017-11-16 10:08:31 +0800

    完善短信模板

--- Detection/src/main/java/com/zhongda/detection/core/utils/sms/SmsContentTemplate.java
+++ Detection/src/main/java/com/zhongda/detection/core/utils/sms/SmsContentTemplate.java
@@ -8,13 +8,13 @@ public class SmsContentTemplate {
 	 */
 	public static final int VerifyCode = 49685;
 	/**
-	 * 数据类告警模板Id : 57427  
-	 * 模板内容 : 尊敬的(账号：{1})客户：您的项目于{2}监测到{3}的超过阈值，项目名称：{4}，采集终端：{5}，传感器编号： {6}，当前值{7}，正常范围{8}~{9}。详情请登陆中大检测在线监测服务平台查看。谢谢 ！
+	 * 数据类告警模板Id : 57454  
+	 * 模板内容 : 尊敬的(账号：{1})客户：您的项目于{2}监测到{3}数据超过阈值，项目名称：{4}，采集终端：{5}，传感器编号： {6}，当前值{7}，正常值范围{8}~{9}。详情请登陆中大检测在线监测服务平台查看。谢谢 ！
 	 */
-	public static final int ALARM_DATA_MESSAGE = 57427;
+	public static final int ALARM_DATA_MESSAGE = 57454;
 	/**
-	 * 设备类告警模板Id : 57429  
+	 * 设备类告警模板Id : 57453  
 	 * 模板内容 : 尊敬的(账号：{1})客户：您好！您的项目于{2}监测到{3}设备类告警信息。项目名称：{4}，采集器终端： {5}， 传感器编号：{6}，{7}，详情请点击中大检测在线检测服务平台查看，谢谢！
 	 */
-	public static final int ALARM_DEVICE_MESSAGE = 57429;
+	public static final int ALARM_DEVICE_MESSAGE = 57453;
 }
